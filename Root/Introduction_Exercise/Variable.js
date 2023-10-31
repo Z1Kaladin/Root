26bccb0b06d43c6199459a3fd14d5b0c28fe84bb
Z1Kaladin <2214039@utrivieramaya.edu.mx>
2023-10-31 17:09:17 -0500

    qw

---
+++ Root/Introduction_Exercise/Variable.js
@@ -0,0 +1,31 @@
+// Comments can make code readable
+// This is my first JS Code
+
+/*
+Comments can make code readable, easy to reuse, and informative
+*/
+
+const myString = "Hello, World!";
+const myBoolean = true;
+const myUndefined = undefined;
+const myNull = null;
+
+console.log(typeof myString);
+console.log(typeof myBoolean);
+console.log(typeof myUndefined);
+console.log(typeof myNull);
+
+let var1, var2, var3, var4;
+
+let num1 = 42, str1 = "Hello", bool1 = true, obj1 = {};
+
+let firstName1 = "Jose";
+let lastName1 = "Riaño";
+let maritalStatus1 = "Single";
+let country1 = "MX";
+let age1 = 23;
+
+let myAge = 23;
+let yourAge = 30;
+console.log("My age is: " + myAge);
+console.log("Your age is: " + yourAge);
